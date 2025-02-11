75e03fc71b9407ff3a41646fa547887cd4af54ce
gerald <gerald.guiony@yahoo.fr>
2025-02-11 18:11:44 +0100

    Update .editorconfig and EspBoardDefs.h

--- src/EspBoardDefs.h
+++ src/EspBoardDefs.h
@@ -49,23 +49,23 @@
 #	define USER_BTN		FUNC_BTN
 #	define RST_BTN		PORT_POWER
 
-
 #endif
 
 // NodeMCU 1.0 12E
 #ifdef ARDUINO_ESP8266_NODEMCU_ESP12E
 
-#	define D0			16
-#	define D1			5							// I2C Bus SCL (clock)
-#	define D2			4							// I2C Bus SDA (data)
-#	define D3			0
-#	define D4			2 							// Same as "LEDBUILTIN", but inverted logic
-#	define D5			14							// SPI Bus SCK (clock)
-#	define D6			12							// SPI Bus MISO
-#	define D7			13							// SPI Bus MOSI
-#	define D8			15							// SPI Bus SS (CS)
-#	define D9			3 							// RX0 (Serial console)
-#	define D10			1 							// TX0 (Serial console)
+// Defined in esp8266/3.1.2/variants/nodemcu/pins_arduino.h
+// #	define D0			16
+// #	define D1			5							// I2C Bus SCL (clock)
+// #	define D2			4							// I2C Bus SDA (data)
+// #	define D3			0
+// #	define D4			2 							// Same as "LEDBUILTIN", but inverted logic
+// #	define D5			14							// SPI Bus SCK (clock)
+// #	define D6			12							// SPI Bus MISO
+// #	define D7			13							// SPI Bus MOSI
+// #	define D8			15							// SPI Bus SS (CS)
+// #	define D9			3 							// RX0 (Serial console)
+// #	define D10			1 							// TX0 (Serial console)
 
 #	define RST_BTN		D0
 #	define FLASH_BTN	D3
